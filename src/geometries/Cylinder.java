af91148c940e215c344a00b09b1faea544a3039d
d-honig <ortaldanielh@gmail.com>
2021-03-16 11:52:35 +0200

    cylinder test and implementatiom

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,6 +1,8 @@
 package geometries;
 
+import primitives.Point3D;
 import primitives.Ray;
+import primitives.Vector;
 
 
 /**
@@ -35,6 +37,25 @@ public class Cylinder extends Tube {
         return _height;
     }
 
+    /**
+     * overriding getNormal from Geometry interface
+     * @param point - Point3D on the geometry cylinder
+     * @return normal vector of Tube
+     */
+    @Override
+    public Vector getNormal(Point3D point) {
+        //if the vector is contained in one of the cylinder bases than the vector from one point in the base to it
+        //would be orthogonal to the axis ray vector
+        Point3D base1 = _axisRay.getPoint();
+        Point3D base2 = base1.add(_axisRay.getDirection().scale(_height));
+        Vector v1 = point.subtract(base1);
+        Vector v2 = point.subtract(base2);
+        if (v1.dotProduct(_axisRay.getDirection()) == 0 || v2.dotProduct(_axisRay.getDirection()) == 0) {
+            return _axisRay.getDirection();
+        }
+        else
+            return super.getNormal(point);
+    }
     @Override
     public String toString() {
         return "Cylinder{" + super.toString() +
